3fde3bda38d063071ff3d1cf989644f5896b53f6
Kjetil Golid <kjetil.golid@gmail.com>
2018-06-18 02:12:58 +0200

    minor tweaks

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="main-menu">
     <a class="current" href="/"><p class="link">Sketches</p></a>
-    <a href="/images/"><p class="link">Gallery</p></a>
+    <a href="/images"><p class="link">Gallery</p></a>
     <a href="/about"><p class="link">About</p></a>
-    <div style="height:30px"></div>
+    <div style="height:25px"></div>
     <a href="https://generated.bigcartel.com"><p class="link">Store</p></a>
-    <div style="height:30px"></div>
+    <div style="height:25px"></div>
     <a href="https://www.github.com/kgolid"><p class="link">GitHub</p></a>
-    <a href="/https://www.twitter.com/kgolid"><p class="link">Twitter</p></a>
-    <a href="/https://www.instagram.com/kgolid"><p class="link">Instagram</p></a>
+    <a href="https://www.twitter.com/kgolid"><p class="link">Twitter</p></a>
+    <a href="https://www.instagram.com/kgolid"><p class="link">Instagram</p></a>
   </div>
 </template>
 <style>
