6a7fdcc9150ec16bf73a9f72faa2d4d44ddb769f
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2021-02-02 21:24:51 +0100

    update p5

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ module.exports = {
   head: {
     title: pkg.name,
     script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.10/p5.js' },
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.10/addons/p5.dom.js' }
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/p5.js' },
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.3/addons/p5.dom.js' }
     ],
     meta: [
       { charset: 'utf-8' },
