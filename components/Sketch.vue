af78fb94d412c0a34df596d7a012aea757984fb6
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2026-04-21 10:44:46 +0200

    Replace rawgit with jsdelivr for p5ycho sketches

--- components/Sketch.vue
+++ components/Sketch.vue
@@ -10,7 +10,7 @@ export default {
   props: ['path', 'external', 'modular', 'background'],
 
   head() {
-    let script_src = this.external ? this.path : 'https://rawgit.com/kgolid/p5ycho/master/' + this.path;
+    let script_src = this.external ? this.path : 'https://cdn.jsdelivr.net/gh/kgolid/p5ycho@9fb448d/' + this.path;
     return {
       script: [{ src: script_src, type: this.modular ? 'module' : '' }],
       style: [{ cssText: 'body { background-color:' + this.background + ' !important;}', type: 'text/css' }]
